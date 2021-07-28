4427717b38a6fbc6574b67c291523c68101e0cec
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2021-07-29 01:56:26 +0900

    Add files via upload
    html렌더링, jinja 블록

--- flask/flask_app/__init__.py
+++ flask/flask_app/__init__.py
@@ -1,23 +1,18 @@
-from flask import Flask
-from flask_app.routes.main_route import bp as main_bp
+from flask import Flask , render_template
+
 
 app = Flask(__name__)
-app.register_blueprint(main_bp)
 
 
-@app.route('/', methods=['POST']) # ㄱㅣ본주소 + / 
-def index():      #안에 실행할 자료 넣어주고 리턴값으로 변수 지정해줘도 가능 
-    # dict_a = {  }
-    return {'hello' : 'Flask'}, 200 #문자열, 튜플, 딕셔너리 형태 가능 #http status 응답코드 전달
-    # return dict_a
- 
-#라우팅 디폴트값
-@app.route('/user/', defaults={'user_id':0}) # 엔드포인트 설정 안하고싶을때, /user/여기부분에 값이 없을때 지정
-#라우팅 추가 
-@app.route('/user/<user_id>') # 라우팅 엔드포인트 설정해줘야됨 
-def user_index(user_id):
-    return f"Here is your user id : {user_id}"
 
+@app.route('/') # ㄱㅣ본주소 + / 
+def index():
+    # apple = 'apple'  #jinja
+    return render_template('index.html') #알아서 html 파일 찾아줌
+
+@app.route('/main')
+def main():
+    return render_template('main_body.html')
 
 if __name__ == '__main__' :
     app.run(debug=True)
